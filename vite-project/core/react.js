da3b651a974bbb848b9e9eb4691648297965a73c
Ada J <jiang.666de6@gmail.com>
2024-01-14 18:10:13 +0800

    mini fiber

--- vite-project/core/react.js
+++ vite-project/core/react.js
@@ -1,11 +1,10 @@
 /*
  * @Author: Ada J
  * @Date: 2024-01-13 22:00:31
- * @LastEditTime: 2024-01-13 22:48:41
+ * @LastEditTime: 2024-01-14 18:05:42
  * @Description: 
  */
 
-// create vdom
 function createElement(type, props, ...children){
   return {
     type,
@@ -20,28 +19,79 @@ function createTextEl(text){
   return {
     type: 'TEXT_EL',
     props: {
-      text,
+      nodeValue: text,
       children: []
     }
   }  
 }
 
 function render(container, el){
-  let dom = null;
-  if(el.type === 'TEXT_EL'){
-    dom = document.createTextNode(el.props.text);
-  }else{
-    dom = document.createElement(el.type);  
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [el] 
+    }
   }
-  Object.keys(el.props).forEach(prop => {
-    if(prop !== 'children') dom[prop] = el.props[prop];
-  })
-  el.props.children.forEach(child => {
-    child = typeof child === 'string' ? createTextEl(child) : child;
-    render(dom, child);
-  })
-  container.append(dom);
 }
+
+function createDom(type){
+  return type === 'TEXT_EL' ? document.createTextNode('') : document.createElement(type); 
+}
+
+function updateProps(dom, props){
+  Object.keys(props).forEach(prop => {
+    if(prop !== 'children') dom[prop] = props[prop];
+  })
+}
+
+function initChild(fiber){
+  const children = fiber.props.children;
+  let preListNode = null;
+  children.forEach((child, idx) => {
+    child = typeof child === 'string' ? createTextEl(child) : child;
+    let unitListNode = {
+      type: child.type,
+      props: child.props,
+      dom: null,
+      child: null,
+      parent: fiber,
+      sibling: null
+    }
+    if(idx === 0){
+      fiber.child = unitListNode;
+    }else{
+      preListNode.sibling = unitListNode;
+    }
+    preListNode = unitListNode;
+  })
+}
+
+function performUnitOfWork(fiber){
+  if(!fiber.dom){
+    const dom = fiber.dom = createDom(fiber.type);
+    updateProps(dom, fiber.props);   
+    fiber.parent.dom.append(dom);    
+  } 
+  
+  initChild(fiber);
+
+  if(fiber.child) return fiber.child;
+  if(fiber.sibling) return fiber.sibling;
+  if(fiber.parent) return fiber.parent.sibling;
+
+}
+
+let nextUnitOfWork = null;
+function workLoop(deadline){
+  let taskPause = false;
+  while(!taskPause){
+    // run task
+    if(nextUnitOfWork) nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    if(deadline.timeRemaining() < 1) taskPause = true;
+  }
+  requestIdleCallback(workLoop);
+}
+requestIdleCallback(workLoop);
 
 const React = {
   createElement,
